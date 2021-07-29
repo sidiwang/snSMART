13cd355d23a22e8af02b4a570208abe05a43786c
Sidi Wang <sidiwang@Sidis-MacBook-Pro.local>
2021-07-28 23:08:30 -0400

    updated simulation function

--- R/continuous_simulation.R
+++ R/continuous_simulation.R
@@ -2,10 +2,11 @@
 #'
 #' simulate data for snSMART with continuous outcome (non-responders re-randomized)
 #'
-#' @param stage1effects vector of mean stage 1 effect of each treatment e.g. stage1effects<-c(50,60,70)
-#' @param stage2weights first stage response rate of B
-#' @param stagecorrs vector of correlations of stage effects. The first number is the correlation of effects between the same treatment in stage 1 and 2, and the second number is the correlation of effects between different treatments in stage 1 and 2. e.g. stagecorrs <- c(0.8, 0.3)
-#' @param variance variance of stage 1 effect among patients
+#' @param treatInfo Treatment distribution information. Each row represents one treatment, format: (mean of treatment effects, sd of treatment effects, c(priming effect on trt A,  trt B, trt C)), see example for detail
+#' @param treatCors Treatment correlations matrix. Format example:
+#' [[ (A,A) (A,B) (A,C)
+#'    (B,A) (B,B) (B,C)
+#'    (C,A) (C,B) (C,C) ]], (A,B) denotes the treatment correlation between first stage treatment A and second stage treatment B
 #' @param n vector of number of patients on each treatment e.g. n <- c(100, 100, 100)
 #' @param stay.ethical numerical value, if stage 1 outcome is bigger than `stay.ethical` value, patient has probability of 1 of staying on the same treatment in stage 2
 #' @param switch.safety numerical value, if stage 1 outcome is smaller than `switch.safety` value, patient has probability of 0 of staying on the same treatment in stage 2
@@ -14,10 +15,13 @@
 #' @return The simulated dataset with 7 variables: patient id, treatment 1, stage 1 outcome, probability of stay on the same treatment, realization of staying flag (1 = stay on the same treatment, 0 = rerandomization in stage 2), treatment 2, stage 2 outcome
 #'
 #' @examples
-#' stage1effects<-c(50,60,70)
-#' stage2weights<-c(.2, .8, 5)
-#' stagecorrs<-c(.8,.3)
-#' variance<-10
+#' treat.a<-c(70, 15, c(0,0,0))
+#' treat.b<-c(50, 10, c(0,0,0))
+#' treat.c<-c(60, 12, c(0,0,0))
+#' treatInfo = rbind(treat.a, treat.b, treat.c)
+#'
+#' treatCors<-diag(.9, 3)
+#'
 #' switch.safety<-NULL
 #' stay.ethical<- NULL
 
@@ -26,7 +30,7 @@
 #' nc<-100
 #' n<-c(na,nb,nc)
 
-#' trialData = trialDataGen(stage1effects, stage2weights, stagecorrs, variance, n)
+#' trialData = trialDataGen(treatInfo, treatCors, n)
 #'
 #' @references
 #' Hartman, H., Tamura, R.N., Schipper, M.J. and Kidwell, K.M., 2021. Design and analysis considerations for utilizing a mapping function in a small sample,
@@ -37,16 +41,16 @@
 
 library(tidyr)
 
-trialDataGen = function(stage1effects, stage2weights, stagecorrs, variance, n,
+trialDataGen = function(treatInfo, treatCors, n,
                         stay.ethical = NULL, switch.safety = NULL,
                         wideForm = TRUE){
 
   #number of treatments based on input values
-  n.trt<-length(stage1effects)
+  n.trt<-dim(treatInfo)[1]
 
-  stage1outcome = c()
-  for(i in 1:n.trt){
-    stage1outcome = c(stage1outcome, rnorm(n[i], mean=stage1effects[i], sd=sqrt(variance)))
+  stage1outcome = rnorm(n[1], mean=treatInfo[1,1], sd=treatInfo[1,2])
+  for(i in 2:n.trt){
+    stage1outcome = c(stage1outcome, rnorm(n[i], mean=treatInfo[i,1], sd=treatInfo[i,2]))
   }
 
   #probability of staying on same trt for each patient
@@ -63,26 +67,35 @@ trialDataGen = function(stage1effects, stage2weights, stagecorrs, variance, n,
   #realization of staying for each patient
   stay<-rbinom(sum(n),1,pstay)
   #vector of rerandomization treatments
-  rerand = c()
+  rerand = sample(2:n.trt,n[1],replace=T)
   trt.vect=1:n.trt
-  for(i in 1:n.trt){
+  for(i in 2:n.trt){
     rerand = c(rerand, sample(trt.vect[!trt.vect == i],n[i],replace=T))
   }
-  trt1<-c()
-  for(i in 1:n.trt){
+  trt1<-rep(1, n[1])
+  for(i in 2:n.trt){
     trt1 <- c(trt1, rep(i, n[i]))
   }
   #realization of trt2 for each patient
   trt2<-ifelse(stay==1, trt1, rerand)
 
-  stage2outcome<-c()
-  for(i in 1:sum(n)){
-    stage2outcomeDistn = condMVNorm::condMVN(mean=c(stage1effects[trt1[i]],stage2weights[1]*stage1effects[trt1[i]]+stage2weights[2]*stage1effects[trt2[i]] + stage2weights[3]*stay[i]),
-                                 sigma = variance * matrix(c(1,
-                                                             stagecorrs[1]*stay[i]+ stagecorrs[2]*(1-stay[i]),
-                                                             stagecorrs[1]*stay[i]+ stagecorrs[2]*(1-stay[i]),
-                                                             1),
-                                                           nrow=2),
+  stage2outcomeDistn = condMVN(mean=c(treatInfo[trt1[1],1],treatInfo[trt2[1],1]+treatInfo[trt1[1],trt2[1]+2]),
+                               sigma = matrix(c(treatInfo[trt1[1],2],
+                                                treatCors[trt1[1], trt2[1]],
+                                                treatCors[trt1[1], trt2[1]],
+                                                treatInfo[trt2[1],2]),
+                                              nrow=2),
+                               dependent.ind = 2,
+                               given.ind = 1,
+                               X.given = stage1outcome[1])
+  stage2outcome = rnorm(1, stage2outcomeDistn$condMean,stage2outcomeDistn$condVar)
+  for(i in 2:sum(n)){
+    stage2outcomeDistn = condMVN(mean=c(treatInfo[trt1[i],1],treatInfo[trt2[i],1]+treatInfo[trt1[i],trt2[i]+2]),
+                                 sigma = matrix(c(treatInfo[trt1[i],2],
+                                                  treatCors[trt1[i], trt2[i]],
+                                                  treatCors[trt1[i], trt2[i]],
+                                                  treatInfo[trt2[i],2]),
+                                                nrow=2),
                                  dependent.ind = 2,
                                  given.ind = 1,
                                  X.given = stage1outcome[i])
@@ -90,7 +103,7 @@ trialDataGen = function(stage1effects, stage2weights, stagecorrs, variance, n,
   }
 
   id= seq(1:sum(n))
-  trial.data=data.frame(id, trt1, stage1outcome, pstay, stay, trt2, stage2outcome)
+  trial.data=data.frame(id, trt1, stage1outcome, pstay, stay, trt2, stage2outcome, stage1outcome)
 
   if(wideForm == FALSE){
     trial.data.long = trial.data %>%
