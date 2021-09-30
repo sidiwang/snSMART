3b583d9fed40958577fbc88d545f703018706c44
Sidi Wang <sidiwang@0587384591.wireless.umich.net>
2021-09-30 12:18:15 -0400

    edited continuous snSMART according to a different file Holly sent me

--- R/c_trialDataGen.R
+++ R/c_trialDataGen.R
@@ -2,11 +2,10 @@
 #'
 #' simulate data for snSMART with continuous outcome (3 active treatments, re-randomization depends on continuous outcome at stage 1, continuous outcomes). This function generates only 1 dataset. Not to be confused with \code{\link{c_trialSim}}
 #'
-#' @param treatInfo Treatment distribution information. Each row represents one treatment, format: (mean of treatment effects, standard deviation of treatment effects, \code{c(priming effect on trt A,  trt B, trt C))}, see example for detail
-#' @param treatCors Treatment correlations matrix. Format example:
-#' \code{[[ (A,A) (A,B) (A,C) ],
-#'    [ (B,A) (B,B) (B,C) ],
-#'    [ (C,A) (C,B) (C,C) ]]}, \code{(A,B)} denotes the treatment correlation between first stage treatment A and second stage treatment B
+#' @param stage1effects a vector of 3 values (`mean effect of treatment A`, `mean effect of treatment B`, `main effect of treatment C`). e.g. `stage1effects = c(40, 50, 70)`
+#' @param stage2weights a vector of 3 values (`weight of stage I`, `weight of stage II`, `weight of staying on the same treatment`). The second stage mean outcome is a weighted average of the treatment effects from stage one and stage two with an additional effect if the patient stays on the same treatment. e.g. `stage2weights = c(0.2, 0.7, 0.3)`
+#' @param stagecorrs a vector of 2 values (`correlations between two stages if patient stays on the same treatment`, `correlations between two stages if patent doesn't stay on the same treatment`). This allows those who stay on the same treatmetn to have a different correlation between stage one and stage two outcomes than those who switch treatments.
+#' @param variance the variance of the treatment effects
 #' @param n vector of number of patients on each treatment, e.g. \code{n <- c(100, 100, 100)}
 #' @param stay.ethical numerical value, if stage 1 outcome is bigger than `stay.ethical` value, patient has probability of 1 of staying on the same treatment in stage 2
 #' @param switch.safety numerical value, if stage 1 outcome is smaller than `switch.safety` value, patient has probability of 0 of staying on the same treatment in stage 2
@@ -15,22 +14,20 @@
 #' @return The simulated dataset with 7 variables: \code{patient id, treatment 1, stage 1 outcome, probability of stay on the same treatment, realization of staying flag (1 = stay on the same treatment, 0 = rerandomization in stage 2), treatment 2, stage 2 outcome}
 #'
 #' @examples
-#' treat.a<-c(70, 15, c(0,0,0))
-#' treat.b<-c(50, 10, c(0,0,0))
-#' treat.c<-c(60, 12, c(0,0,0))
-#' treatInfo = rbind(treat.a, treat.b, treat.c)
-#'
-#' treatCors<-diag(.9, 3)
-#'
+#' stage1effects<-c(50,60,70)
+#' stage2weights<-c(.2, .8, 5)
+#' stagecorrs<-c(.8,.3)
+#' variance<-10
 #' switch.safety<-NULL
 #' stay.ethical<- NULL
-#'
+
+#' #number of patients on each treatment
 #' na<-100
 #' nb<-100
 #' nc<-100
 #' n<-c(na,nb,nc)
 #'
-#' trialData = c_trialDataGen(treatInfo, treatCors, n, wideForm = FALSE)
+#' trialData = c_trialDataGen(stage1effects, stage2weights, stagecorrs, variance, n, wideForm = FALSE)
 #'
 #' @references
 #' Hartman, H., Tamura, R.N., Schipper, M.J. and Kidwell, K.M., 2021. Design and analysis considerations for utilizing a mapping function in a small sample,
@@ -44,17 +41,16 @@
 #'
 
 
-c_trialDataGen = function(treatInfo, treatCors, n,
-                        stay.ethical = NULL, switch.safety = NULL,
-                        wideForm = TRUE){
+c_trialDataGen = function(stage1effects, stage2weights, stagecorrs, variance, n,
+                          stay.ethical = NULL, switch.safety = NULL,
+                          wideForm = TRUE){
 
-  require(tidyr)
   #number of treatments based on input values
-  n.trt<-dim(treatInfo)[1]
+  n.trt<-length(stage1effects)
 
-  stage1outcome = rnorm(n[1], mean=treatInfo[1,1], sd=treatInfo[1,2])
-  for(i in 2:n.trt){
-    stage1outcome = c(stage1outcome, rnorm(n[i], mean=treatInfo[i,1], sd=treatInfo[i,2]))
+  stage1outcome = c()
+  for(i in 1:n.trt){
+    stage1outcome = c(stage1outcome, rnorm(n[i], mean=stage1effects[i], sd=variance))
   }
 
   #probability of staying on same trt for each patient
@@ -71,35 +67,26 @@ c_trialDataGen = function(treatInfo, treatCors, n,
   #realization of staying for each patient
   stay<-rbinom(sum(n),1,pstay)
   #vector of rerandomization treatments
-  rerand = sample(2:n.trt,n[1],replace=T)
+  rerand = c()
   trt.vect=1:n.trt
-  for(i in 2:n.trt){
+  for(i in 1:n.trt){
     rerand = c(rerand, sample(trt.vect[!trt.vect == i],n[i],replace=T))
   }
-  trt1<-rep(1, n[1])
-  for(i in 2:n.trt){
+  trt1<-c()
+  for(i in 1:n.trt){
     trt1 <- c(trt1, rep(i, n[i]))
   }
   #realization of trt2 for each patient
   trt2<-ifelse(stay==1, trt1, rerand)
 
-  stage2outcomeDistn = condMVNorm::condMVN(mean=c(treatInfo[trt1[1],1],treatInfo[trt2[1],1]+treatInfo[trt1[1],trt2[1]+2]),
-                               sigma = matrix(c(treatInfo[trt1[1],2],
-                                                treatCors[trt1[1], trt2[1]],
-                                                treatCors[trt1[1], trt2[1]],
-                                                treatInfo[trt2[1],2]),
-                                              nrow=2),
-                               dependent.ind = 2,
-                               given.ind = 1,
-                               X.given = stage1outcome[1])
-  stage2outcome = rnorm(1, stage2outcomeDistn$condMean,stage2outcomeDistn$condVar)
-  for(i in 2:sum(n)){
-    stage2outcomeDistn = condMVNorm::condMVN(mean=c(treatInfo[trt1[i],1],treatInfo[trt2[i],1]+treatInfo[trt1[i],trt2[i]+2]),
-                                 sigma = matrix(c(treatInfo[trt1[i],2],
-                                                  treatCors[trt1[i], trt2[i]],
-                                                  treatCors[trt1[i], trt2[i]],
-                                                  treatInfo[trt2[i],2]),
-                                                nrow=2),
+  stage2outcome<-c()
+  for(i in 1:sum(n)){
+    stage2outcomeDistn = condMVN(mean=c(stage1effects[trt1[i]],stage2weights[1]*stage1effects[trt1[i]]+stage2weights[2]*stage1effects[trt2[i]] + stage2weights[3]*stay[i]),
+                                 sigma = variance * matrix(c(1,
+                                                             stagecorrs[1]*stay[i]+ stagecorrs[2]*(1-stay[i]),
+                                                             stagecorrs[1]*stay[i]+ stagecorrs[2]*(1-stay[i]),
+                                                             1),
+                                                           nrow=2),
                                  dependent.ind = 2,
                                  given.ind = 1,
                                  X.given = stage1outcome[i])
--- R/c_trialSim.R
+++ R/c_trialSim.R
@@ -2,38 +2,27 @@
 #'
 #' assess the characteristics of trial design based on datasets simulated by \code{\link{c_trialDataGen}}
 #'
-#' @param treatInfo Treatment distribution information. Each row represents one treatment, format: (mean of treatment effects, sd of treatment effects, \code{c(priming effect on trt A,  trt B, trt C)}), see example for detail
-#' @param treatCors Treatment correlations matrix. Format example:
-#' \code{[[ (A,A) (A,B) (A,C) ],
-#'    [ (B,A) (B,B) (B,C) ],
-#'    [ (C,A) (C,B) (C,C) ]]}, \code{(A,B)} denotes the treatment correlation between first stage treatment A and second stage treatment B
+#' @param stage1effects a vector of 3 values (`mean effect of treatment A`, `mean effect of treatment B`, `main effect of treatment C`). e.g. `stage1effects = c(40, 50, 70)`
+#' @param stage2weights a vector of 3 values (`weight of stage I`, `weight of stage II`, `weight of staying on the same treatment`). The second stage mean outcome is a weighted average of the treatment effects from stage one and stage two with an additional effect if the patient stays on the same treatment. e.g. `stage2weights = c(0.2, 0.7, 0.3)`
+#' @param stagecorrs a vector of 2 values (`correlations between two stages if patient stays on the same treatment`, `correlations between two stages if patent doesn't stay on the same treatment`). This allows those who stay on the same treatmetn to have a different correlation between stage one and stage two outcomes than those who switch treatments.
+#' @param variance the variance of the treatment effects
 #' @param n vector of number of patients on each treatment, e.g. \code{n <- c(100, 100, 100)}
-#' @param nsim number of simulations. Use the `c_trialDataGen` function to generate \code{nsim} simulated datasets
+#' @param nsim number of simulations. Use the \code{\link{c_trialDataGen}} function to generate \code{nsim} simulated datasets
 #' @param stay.ethical numerical value, if stage 1 outcome is bigger than `stay.ethical` value, patient has probability of 1 of staying on the same treatment in stage 2
 #' @param switch.safety numerical value, if stage 1 outcome is smaller than `switch.safety` value, patient has probability of 0 of staying on the same treatment in stage 2
 
 #' @return
 #' \describe{
 #' \item{switch.safety}{input by user or estimated based on data if `switch.safety` = NULL}
-#'
 #' \item{stay.ethical}{input by user or estimated based on data if `stay.ethical` = NULL}
-#'
 #' \item{stage1Eff}{average simulated stage 1 treatment effect }
-#'
 #' \item{stage2EFF}{average simulated stage 2 treatment effect }
-#'
 #' \item{stage2best}{percentage of patients assigned to the best treatment in stage 2; overallbest: percentage of patients assigned to the best treatment at least once in the trial}
-#'
 #' \item{stage2worst}{percentage of patients assigned to the worst treatment in stage 2; overallworst: percentage of patients assigned to the worst treatment at least once in the trial}
-#'
 #' \item{improve}{percentage of patients received an improved treatment outcome in stage 2}
-#'
 #' \item{switchBetterOrStayedBest}{percentage of patients received more effective treatments in stage 2 or stayed on the best treatment throughout the trial}
-#'
 #' \item{stayedSame}{percentage of patients received the same treatment in stage 2 after randomization}
-#'
 #' \item{gotBetter}{percentage of patients that received more effective treatments in stage 2}
-#'
 #' \item{gotWorse}{percentage of patients that received less effective treatments in stage 2}
 #' }
 #'
@@ -53,7 +42,7 @@
 #' nc<-100
 #' n<-c(na,nb,nc)
 
-#' k = c_trialSim(treatInfo, treatCors, stay.ethical, switch.safety, n, 100)
+#' k = c_trialSim(stage1effects, stage2weights, stagecorrs, variance, n, 100)
 #'
 #' @references
 #' Hartman, H., Tamura, R.N., Schipper, M.J. and Kidwell, K.M., 2021. Design and analysis considerations for utilizing a mapping function in a small sample,
@@ -69,10 +58,13 @@
 
 
 
-c_trialSim = function(treatInfo, treatCors, stay.ethical = NULL, switch.safety = NULL, n, nsim){
+c_trialSim = function(stage1effects, stage2weights, stagecorrs, variance, stay.ethical = NULL, switch.safety = NULL, n, nsim){
   x<-c()
+
+  treatInfo = rbind(c(stage1effects[1], rep(0, 4)), c(stage1effects[2], rep(0, 4)), c(stage1effects[3], rep(0, 4)))
+
     for(i in 1:nsim){
-    trialData = c_trialDataGen(treatInfo = treatInfo, treatCors = treatCors, stay.ethical = stay.ethical, switch.safety = switch.safety, n = n, wideForm = T)
+    trialData = c_trialDataGen(stage1effects = stage1effects, stage2weights = stage2weights, stagecorrs = stagecorrs, variance = variance, n = n, stay.ethical = stay.ethical, switch.safety = switch.safety, wideForm = T)
     stage1EFF = mean(trialData$stage1outcome)
     stage2EFF = mean(trialData$stage2outcome)
 
