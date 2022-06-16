5ec13a41970eb17c60ccc4ce41b4a9974d7cae7d
Sidi Wang <sidiwang@0587671413.vpn.umich.net>
2022-06-16 16:51:08 -0400

    edited dataset name

--- R/data.R
+++ R/data.R
@@ -1,13 +1,13 @@
 
-#' Patient Entry
+#' group sequential data look 1
 #'
 #' sample dataset of group sequential trial design snSMART, can be used for interim analysis
 #'
-#' @name patient_entry
+#' @name groupseqDATA_look1
 #' @docType data
 #' @keywords data
 #' @examples
-#' mydata = patient_entry
+#' mydata = groupseqDATA_look1
 #'
 #' result1 = group_seq(data = mydata, rule.type = 1, interim = TRUE, drop_threshold=0.5, NUM_ARMS = 3, pi_prior_dist = "beta", pi_prior.a =  c(0.4,0.4,0.4), pi_prior.b = c(1.6, 1.6, 1.6), beta0_prior_dist = "beta",
 #' beta0_prior.a = 1.6, beta0_prior.b = 0.4, beta1_prior_dist = "pareto", beta1_prior.a = 3, beta1_prior.c = 1, MCMC_SAMPLE = 60000, BURN.IN = 10000,
@@ -20,14 +20,14 @@
 
 NULL
 
-#' Patient Entry (full)
+#' group sequential full data
 #'
 #' sample dataset of group sequential trial design snSMART, can be used for final analysis
-#' @name patient_entry_full
+#' @name groupseqDATA_full
 #' @docType data
 #' @keywords data
 #' @examples
-#' mydata = patient_entry_full
+#' mydata = groupseqDATA_full
 #' result3 = group_seq(data = mydata, rule.type = 2, interim = FALSE, NUM_ARMS = 3, pi_prior_dist = "beta", pi_prior.a =  c(0.4,0.4,0.4), pi_prior.b = c(1.6, 1.6, 1.6), beta0_prior_dist = "beta",
 #' beta0_prior.a = 1.6, beta0_prior.b = 0.4, beta1_prior_dist = "pareto", beta1_prior.a = 3, beta1_prior.c = 1, MCMC_SAMPLE = 60000, BURN.IN = 10000,
 #' n_MCMC_chain = 1, ci = 0.95, DTR = TRUE)
@@ -63,11 +63,11 @@ NULL
 #' Data Mapping Function
 #'
 #' sample dataset of snSMART (mapping function) with continuous outcomes
-#' @name data_c
+#' @name trialDataMF
 #' @docType data
 #' @keywords data
 #' @examples
-#' mydata = data_dose
+#' mydata = trialDataMF
 #' BJSM_result = BJSM_c(data = trialData, xi_prior.mean = c(50, 50, 50),
 #' xi_prior.sd = c(50, 50, 50), phi3_prior.sd = 20, n_MCMC_chain = 1,
 #' n.adapt = 1000, MCMC_SAMPLE = 5000, ci = 0.95, n.digits = 5)
